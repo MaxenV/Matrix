e1181b76753f8a0011761d0d19ed11722f60dd09
MaxenV <maxen2001@gmail.com>
2022-06-11 16:52:34 +0200

    Move check function from class outside
    Function which check correct record of matrix was moved outside class

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
-from matrix_lib import Matrix
+from matrix_lib import *
 
-matrix1 = Matrix([[4, 4, 3, 5], [7, 8, 5, 7], [2, 7, 3, 1], [6, 3, 4, 5]])
-matrix2 = Matrix([[4, 4, 3, 5], [7, 8, 5, 7], [2, 7, 3, 1], [6, 3, 4, 5]])
-
-matrix3 = Matrix([[4, 4, 3], [7, 8, 5], [2, 7, 3], [6, 3, 4]])
-
-matrix4 = Matrix([[5, 3, 6, 2], [7, 4, 6, 8], [1, 5, 3, 6]])
-
-# matrixI = Matrix([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
-
-
-matrix1.show_matrix()
-matrix1.transpose()
-print()
-matrix1.show_matrix()
-
-print()
-print()
-matrix3.show_matrix()
-matrix3.transpose()
-print()
-matrix3.show_matrix()
-
-print()
-print()
-matrix4.show_matrix()
-matrix4.transpose()
-print()
-matrix4.show_matrix()
+# matrix1 = Matrix([[4, 4, 3, 5], [7, 8, 5, 7], [2, 7, 3, 1], [6, 3, 4, 5]])
+# matrix2 = Matrix([[4, 4, 3, 5], [7, 8, 5, 7], [2, 7, 3, 1], [6, 3, 4, 5]])
+#
+# matrix3 = Matrix([[4, 4, 3], [7, 8, 5], [2, 7, 3], [6, 3, 4]])
+#
+# matrix4 = Matrix([[5, 3, 6, 2], [7, 4, 6, 8], [1, 5, 3, 6]])
+#
+# # matrixI = Matrix([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+#
+#
+# matrix1.show_matrix()
+# matrix1.transpose()
+# print()
+# matrix1.show_matrix()
+#
+# print()
+# print()
+# matrix3.show_matrix()
+# matrix3.transpose()
+# print()
+# matrix3.show_matrix()
+#
+# print()
+# print()
+# matrix4.show_matrix()
+# matrix4.transpose()
+# print()
+# matrix4.show_matrix()
--- matrix_lib.py
+++ matrix_lib.py
@@ -5,7 +5,7 @@ class Matrix:
     def __init__(self, *mtx):
         if len(mtx) > 0:
             if type(mtx[0]) == list:
-                if self.check_if_correct(mtx[0]):
+                if check_if_correct(mtx[0]):
                     self.matrix = mtx[0]
                     self.columns = len(self.matrix[0])
                     self.rows = len(self.matrix)
@@ -29,7 +29,8 @@ class Matrix:
             self.matrix = list()
 
     def fill_matrix_asks_user(self):
-        return [[int(input(f"Type number on {w}{c}: ")) for c in range(1, self.columns+1)] for w in range(1, self.rows+1)]
+        return [[int(input(f"Type number on {w}{c}: ")) for c in range(1, self.columns+1)]
+                for w in range(1, self.rows+1)]
 
     def fill_matrix_random(self, minimum, maximum):
         return [[randint(minimum, maximum) for _ in range(1, self.columns+1)] for _ in range(1, self.rows+1)]
@@ -39,21 +40,6 @@ class Matrix:
             for num in row:
                 print(num, end="\t")
             print()
-
-
-    @staticmethod
-    def check_if_correct(mtx):
-        if type(mtx[0]) == list:
-            num_cols = len(mtx[0])
-            for row in mtx:
-                if type(row) == list and num_cols != len(row):
-                    return False
-                for num in row:
-                    if not (type(num) == int or type(num) == float):
-                        return False
-        else:
-            return False
-        return True
 
     def check_if_square(self):
         return self.columns == self.rows
@@ -100,3 +86,18 @@ class Matrix:
 
         self.columns = self.rows
         self.rows = new_rows
+
+
+def check_if_correct(mtx):
+    if type(mtx[0]) == list:
+        num_cols = len(mtx[0])
+        for row in mtx:
+            if type(row) == list and num_cols != len(row):
+                return False
+            for num in row:
+                if not (type(num) == int or type(num) == float):
+                    return False
+    else:
+        return False
+    return True
+
